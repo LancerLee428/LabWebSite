a36bd4fb6dfde511e81c8e838712c50ce713cf78
LancerLee <hhu_lijing123@163.com>
2023-05-04 22:14:57 +0800

    feat: 团队导师时间戳组件做样式穿透

--- src/assets/info/resumeInfo.ts
+++ src/assets/info/resumeInfo.ts
@@ -55,28 +55,111 @@ const RESUMEINFO = [
     id: "swb-resume",
     key: "2",
     imgSrc:
-      "https://dcxy.cumtb.edu.cn/__local/6/0A/42/DBEB8C7DA479DBE2753C9B1CB2D_12FC9C1D_32144F.jpg",
+      "https://dcxy.cumtb.edu.cn/__local/8/42/BA/4FBF7C5BAB7423A245F04CC9C8B_55907B2E_3175A4.jpg",
     name: "孙文彬教授",
-    email: "zxs@cumtb.edu.cn",
-    content: [],
+    email: "swb@cumtb.edu.cn",
+    content: [
+      {
+        title: "2000",
+        content1: "毕业于西南交通大学土木工程学院",
+        content2: "获测绘工程专业工学学士学位",
+      },
+      {
+        title: "2003",
+        content1: "毕业于西南交通大学土木工程学院",
+        content2: "获大地测量学与测绘工程专业工学硕士学位",
+      },
+      {
+        title: "2007",
+        content1: "毕业于中国矿业大学（北京）",
+        content2: "获地图制图学与地理信息工程专业工学博士学位",
+      },
+      {
+        title: "2007",
+        content1: "中国矿业大学（北京）资源与安全工程学院",
+        content2: "担任讲师",
+      },
+      {
+        title: "2011",
+        content1: "晋升副教授",
+      },
+      {
+        title: "2017",
+        content1: "晋升教授",
+      },
+      {
+        title: "目前",
+        content1: "担任遥感与地理信息系主任",
+      },
+    ],
   },
   {
     id: "fdq-resume",
     key: "3",
     imgSrc:
-      "https://dcxy.cumtb.edu.cn/__local/6/0A/42/DBEB8C7DA479DBE2753C9B1CB2D_12FC9C1D_32144F.jpg",
+      "https://dcxy.cumtb.edu.cn/__local/0/11/50/74F11CB338E05A171C99BA9EE2A_4C25BE88_1C6C7.jpg",
     name: "范德芹副研究员",
-    email: "zxs@cumtb.edu.cn",
-    content: [],
+    email: "kinly129@163.com",
+    content: [
+      {
+        title: "2007",
+        content1: "毕业于东北林业大学",
+        content2: "获地理信息系统专业学士学位",
+      },
+      {
+        title: "2011",
+        content1: "毕业于中国矿业大学（北京）",
+        content2: "获摄影测量与遥感专业学硕士学位",
+      },
+      {
+        title: "2014",
+        content1: "毕业于北京师范大学",
+        content2: "获地图学与地理信息系统专业博士学位",
+      },
+      {
+        title: "2014-2017",
+        content1: "中国矿业大学（北京）测绘科学与技术学科",
+        content2: "博士后",
+      },
+      {
+        title: "2022",
+        content1: "晋升副研究员",
+      },
+    ],
   },
   {
     id: "gw-resume",
     key: "4",
     imgSrc:
-      "https://dcxy.cumtb.edu.cn/__local/6/0A/42/DBEB8C7DA479DBE2753C9B1CB2D_12FC9C1D_32144F.jpg",
+      "https://dcxy.cumtb.edu.cn/__local/2/6B/5E/879C615886E0BC79C928BF8098B_F7734741_3EA035.jpg",
     name: "郭伟",
-    email: "zxs@cumtb.edu.cn",
-    content: [],
+    email: "weiguo@cumtb.edu.cn",
+    content: [
+      {
+        title: "2007",
+        content1: "毕业于东北林业大学",
+        content2: "获地理信息系统专业学士学位",
+      },
+      {
+        title: "2011",
+        content1: "毕业于中国矿业大学（北京）",
+        content2: "获摄影测量与遥感专业学硕士学位",
+      },
+      {
+        title: "2014",
+        content1: "毕业于北京师范大学",
+        content2: "获地图学与地理信息系统专业博士学位",
+      },
+      {
+        title: "2014-2017",
+        content1: "中国矿业大学（北京）测绘科学与技术学科",
+        content2: "博士后",
+      },
+      {
+        title: "2022",
+        content1: "晋升副研究员",
+      },
+    ],
   },
 ];
 
