ae8b521a08807f4b18400bb3c0a78e48aa6f6e8b
LancerLee <hhu_lijing123@163.com>
2023-05-07 23:31:01 +0800

    feat: 英文标题

--- src/components/Content/Direction/DirectionCar.tsx
+++ src/components/Content/Direction/DirectionCar.tsx
@@ -4,7 +4,10 @@ import DirectCar from "@/common/DirectCar";
 const DirectionCar = () => {
   return (
     <>
-      <section className="dc-title">研究方向</section>
+      <section className="dc-title">
+        <section className="dc-head-en">RESEARCH DIRECTION</section>
+        <section className="dc-head">研究方向</section>
+      </section>
       <section className="dc-layout">
         {DIRECTION_INFO.map((item) => {
           return <DirectCar {...item} key={item.id} />;
